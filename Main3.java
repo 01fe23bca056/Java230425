517e49431f11d2973028a0bd58ace01478d11ef5
01fe23bca056 <01fe23bca056@kletech.ac.in>
2025-04-23 09:45:28 +0530

    Added total 10 simple java programs

---
+++ Main3.java
@@ -0,0 +1,8 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int a = 12, b = 6;
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        System.out.println("A - B = " + (a - b));
+    }
+}
---
+++ Main8.java
@@ -0,0 +1,7 @@
+public class Main8 {
+    public static void main(String[] args) {
+        int side = 4;
+        int area = side * side;
+        System.out.println("Cube with side " + side + " has area of " + area);
+    }
+}
